3444f77b127249f155d52d5dcc1d0a1cca94b165
Francesco Bonilla <fab@stiga.es>
2024-10-30 14:00:36 +0100

    feat: Añadida persistencia de datos en by-capital y en by-country

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesServiceService } from '../../services/countries-service.service';
 
@@ -7,13 +7,20 @@ import { CountriesServiceService } from '../../services/countries-service.servic
   templateUrl: './by-country-page.component.html',
   styles: ``,
 })
-export class ByCountryPageComponent {
+export class ByCountryPageComponent implements OnInit {
   // Declare new empty Country array to save the result from the http request
   public countries: Country[] = [];
+
+  public placeholder: string = '';
 
   public isLoaded: boolean = false;
 
   constructor(private CountriesService: CountriesServiceService) {}
+
+  ngOnInit(): void {
+    this.placeholder = this.CountriesService.cacheStore.byCountries.searchTerm;
+    this.countries = this.CountriesService.cacheStore.byCountries.countries;
+  }
 
   searchByCountry(term: string): void {
     this.isLoaded = true;
--- src/app/countries/services/countries-service.service.ts
+++ src/app/countries/services/countries-service.service.ts
@@ -1,7 +1,9 @@
 import { Country } from '../interfaces/country';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, delay, map, Observable, of } from 'rxjs';
+import { catchError, delay, map, Observable, of, tap } from 'rxjs';
+import { CacheStore } from '../interfaces/cache-store.interface';
+import { Region } from '../interfaces/region.type';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +11,24 @@ import { catchError, delay, map, Observable, of } from 'rxjs';
 export class CountriesServiceService {
   private apiURL: string = 'https://restcountries.com/v3.1';
 
-  constructor(private HttpClient: HttpClient) {}
+  public cacheStore: CacheStore = {
+    byCapital: {
+      searchTerm: '',
+      countries: [],
+    },
+    byCountries: {
+      searchTerm: '',
+      countries: [],
+    },
+    byRegion: {
+      region: '',
+      countries: [],
+    },
+  };
+
+  constructor(private HttpClient: HttpClient) {
+    console.log(this.cacheStore);
+  }
 
   searchCountryByAlphaCode(code: string): Observable<Country | null> {
     const url = `${this.apiURL}/alpha/${code}`;
@@ -34,18 +53,42 @@ export class CountriesServiceService {
     const url = `${this.apiURL}/capital/${capital}`;
 
     // Return the request
-    return this.getHttpRequest(url);
+    return this.getHttpRequest(url).pipe(
+      tap(
+        (countries) =>
+          (this.cacheStore.byCapital = {
+            searchTerm: capital,
+            countries,
+          })
+      )
+    );
   }
 
   searchCountry(country: string): Observable<Country[]> {
     const url = `${this.apiURL}/name/${country}`;
 
-    return this.getHttpRequest(url);
+    return this.getHttpRequest(url).pipe(
+      tap(
+        (countries) =>
+          (this.cacheStore.byCountries = {
+            searchTerm: country,
+            countries,
+          })
+      )
+    );
   }
 
-  searchRegion(region: string): Observable<Country[]> {
+  searchRegion(region: Region): Observable<Country[]> {
     const url = `${this.apiURL}/region/${region}`;
 
-    return this.getHttpRequest(url);
+    return this.getHttpRequest(url).pipe(
+      tap(
+        (countries) =>
+          (this.cacheStore.byRegion = {
+            region: region,
+            countries,
+          })
+      )
+    );
   }
 }
